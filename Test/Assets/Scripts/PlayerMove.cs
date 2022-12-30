5030b835c2c3318f1b7fa5574413ced919e700df
nilbace <nilbace@naver.com>
2022-12-30 23:08:49 +0900

    1차
    빠꾸직전

--- Test/Assets/Scripts/PlayerMove.cs
+++ Test/Assets/Scripts/PlayerMove.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rigidbody2D;
+    public float moveSpeed;
+    public float jumpPower;
+    public float clingCoolTime;
+    public float clingJumpPower;
+    public bool isGround = true;
     void Start()
     {
         
@@ -13,6 +18,50 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        clingCoolTime+=Time.deltaTime;
+        if(Input.GetKey(KeyCode.A) )
+        {
+            rigidbody2D.AddForce(new Vector2(-moveSpeed, 0));
+        }
+
+        if(Input.GetKey(KeyCode.D) )
+        {
+            rigidbody2D.AddForce(new Vector2(moveSpeed, 0));
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpPower));
+            isGround= false;
+        }
+
+        if(rigidbody2D.bodyType == RigidbodyType2D.Static && Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Input.GetKey(KeyCode.A))
+            {
+                rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+                clingCoolTime = 0f;
+                rigidbody2D.AddForce(new Vector2(-clingJumpPower, jumpPower));
+            }
+
+            if(Input.GetKey(KeyCode.D))
+            {
+                rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+                clingCoolTime = 0f;
+                rigidbody2D.AddForce(new Vector2(clingJumpPower, jumpPower));
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Wall") && clingCoolTime > 0.1f)
+        {
+            rigidbody2D.bodyType = RigidbodyType2D.Static;
+        }
+
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
     }
 }
